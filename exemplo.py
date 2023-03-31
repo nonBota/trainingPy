6c2c05c9450e140e3536d87285a5d912c02fe157
Srbottine <45329014+Srbottine@users.noreply.github.com>
2023-03-31 11:59:44 +0000

    exemplos if

--- exemplo.py
+++ exemplo.py
@@ -1,11 +1,29 @@
-tempo = int(input('Quantos anos tem seu carro?'))
-if tempo<=3:
-    print('carro novo')
-else:
-    print('carro velho')
-print('--FIM--')
+#tempo = int(input('Quantos anos tem seu carro?'))
+#if tempo<=3:
+    #print('carro novo')
+#else:
+    #print('carro velho')
+#print('--FIM--')
 
 
 #tempo = int(input('Quantos anos tem seu carro?'))
 #print('carro novo'if tempo<=3 else'carro velho')
 #print('--FIM--') 
+
+
+#nome = str(input('nome:'))
+#if nome =='pidao' :
+    #print('aaaaaaaauuuuuuuu')
+#else:
+    #print('viado') 
+#print('bom dia viado')
+
+
+n1 = float(input('digite sua primeira nota '))
+n2 = float(input('digite sua segunda nota '))
+m = (n1 + n2)/2
+print("a sua media foi {:.1f}".format(m))
+if m >=6.0:
+    print('sua nome e boa ')
+else:
+    print('sua media e ruim')
