5d7b42d35f87a3f43bcdf2a537c9b9b14b7dbacb
Srbottine <45329014+Srbottine@users.noreply.github.com>
2023-09-11 16:55:10 +0000

    Ex36 concluido

--- World 2/module A/exercise/ex36.py
+++ World 2/module A/exercise/ex36.py
@@ -1,4 +1,13 @@
 house = float(input('digite o valor da casa: '))
 salary = float(input('digite o valor do seu salario: '))
-quota = int(input('em quantas parcelas: '))
+years = int(input('em quantas parcelas: '))
 
+prestacao = house / (years * 12 )
+min = salary * 30 / 100
+print('para pagar a casa de R${:.2f} em {} anos'.format(house, years), end='')
+print('  a prestação sera de R${:.2f}'.format(prestacao))
+
+if prestacao <= min:
+    print('emprestimo concedido')
+else:
+    print('negado')
